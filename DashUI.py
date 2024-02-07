8f0b9dace578d0b28e52dd83e64598af4cf68d0d
MRMillikan <millika6@msu.edu>
2024-02-07 14:00:34 -0500

    Update DashUI.py

--- DashUI.py
+++ DashUI.py
@@ -2,6 +2,7 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 import json
+from dash_selectable import DashSelectable
 
 app = dash.Dash(__name__)
 
@@ -21,7 +22,12 @@ app.layout = html.Div([
         id='text-input',
         value=sentences[current_sentence_index],
         style={'width': '100%', 'height': 200},
+
     ),
+        DashSelectable(
+            id="dash-selectable",
+            children=[html.P(sentences[current_sentence_index]), html.P(id="output")],
+        ),
     html.Button('Increase', id='increase-btn', n_clicks=0),
     html.Button('Decrease', id='decrease-btn', n_clicks=0),
     html.Button('Save Relation', id='save-btn', n_clicks=0),
@@ -30,6 +36,17 @@ app.layout = html.Div([
     dcc.Store(id='memory-store', storage_type='local'),
     ])
 ])
+
+#test
+@app.callback(Output("output", "children"), [Input("dash-selectable", "selectedValue")])
+def display_output(value):
+    text = ""
+    if value:
+        text = value
+
+    return "You have selected: {}".format(text)
+
+
 
 @app.callback(
     [Output('text-input', 'value'),
