57e49a8ded21194e5e05ac03a44035fbdc8b9d98
MRMillikan <millika6@msu.edu>
2024-02-08 17:26:48 -0500

    Update DashUI.py

--- DashUI.py
+++ DashUI.py
@@ -19,15 +19,25 @@ current_sentence_index = 0
 app.layout = html.Div([
     html.Div([
         dcc.Textarea(
-        id='text-input',
-        value=sentences[current_sentence_index],
-        style={'width': '100%', 'height': 200},
+            id='text-input',
+            value=sentences[current_sentence_index],
+            style={'width': '100%', 'height': 200},
 
-    ),
+        ),
         DashSelectable(
             id="dash-selectable",
-            children=[html.P(sentences[current_sentence_index]), html.P(id="output")],
+            children=[html.P(id="sentence"), html.P(id="output")],
         ),
+        html.Br(),
+        dcc.Textarea(
+            id = 'my-source',
+            value = "Source: ",
+            style={'width': '100%', 'height': 200},
+            disabled = True,
+        ),
+        #html.div([
+
+        #]),
     html.Button('Increase', id='increase-btn', n_clicks=0),
     html.Button('Decrease', id='decrease-btn', n_clicks=0),
     html.Button('Save Relation', id='save-btn', n_clicks=0),
@@ -38,7 +48,8 @@ app.layout = html.Div([
 ])
 
 #test
-@app.callback(Output("output", "children"), [Input("dash-selectable", "selectedValue")])
+@app.callback(Output("output", "children"),
+              [Input("dash-selectable", "selectedValue")])
 def display_output(value):
     text = ""
     if value:
@@ -50,7 +61,8 @@ def display_output(value):
 
 @app.callback(
     [Output('text-input', 'value'),
-     Output('memory-store', 'data')],
+     Output('memory-store', 'data'),
+     Output('sentence','children')],
     [Input('next-btn', 'n_clicks')],
     [State('text-input', 'value'),
      State('memory-store', 'data')]
@@ -58,11 +70,24 @@ def display_output(value):
 def next_sentence(n_clicks, current_text, stored_data):
     current_sentence_index = int(n_clicks)
     if current_sentence_index < len(sentences):
-        return sentences[current_sentence_index], stored_data
+        return sentences[current_sentence_index], stored_data,sentences[current_sentence_index]
     else:
-        return current_text, stored_data
+        return current_text, stored_data, current_text
 
 # Similar callbacks for increase, decrease, save, reset, etc.
+
+@app.callback(
+    [Output('my-source','value')],
+    [Input('increase-btn', 'n_clicks')],
+    [State("dash-selectable", "selectedValue"),
+     State('my-source','value')]
+)
+
+def increase(n_clicks, selected_data,value):
+    if selected_data:
+        return "Source: {}".format(selected_data)
+    else:
+        return value
 
 if __name__ == '__main__':
     app.run_server(debug=True)
